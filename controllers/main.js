27f02ba041d1d7bf05f30178d4c73e155b91f412
Alp Serin <alpsrn@gmail.com>
2023-11-21 23:41:54 +0300

    create jwt token and send it back to the user

--- controllers/main.js
+++ controllers/main.js
@@ -8,8 +8,8 @@
 
 require("dotenv").config();
 const request = require("request");
-const API_KEY = process.env.NINJA_API_KEY;
 
+const jwt = require("jsonwebtoken");
 const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
@@ -17,14 +17,21 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomAPIError("Please provide username and password", 400);
   }
-  res.send("Fake route");
+
+  const id = new Date().getDate(); // Just for demo, normally provided by DB!!
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = (req, res) => {
   request.get(
     {
       url: "https://api.api-ninjas.com/v1/quotes",
-      headers: { "X-Api-Key": API_KEY },
+      headers: { "X-Api-Key": process.env.NINJA_API_KEY },
     },
     (error, response, body) => {
       body = body.toString("utf8");
