7c373f64053c50d2c6aa1bc1bf7f9bc77cc2facd
Alp Serin <alpsrn@gmail.com>
2023-11-21 23:18:40 +0300

    add validation for username and password

--- controllers/main.js
+++ controllers/main.js
@@ -1,10 +1,23 @@
+// check username and password in post(login) request
+// if exists, create new JWT
+// send back to frontend
+
+// setup authentication so only the request with JWT can access the dashboard
+
+// THIS WILL PROJECT WILL NOT HAVE A DATABASE, WE WILL MANUALLY CHECK THE USERNAME AND THE PASSWORD
+
 require("dotenv").config();
 const request = require("request");
-
 const API_KEY = process.env.NINJA_API_KEY;
 
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
-  res.send("Fake Login/Register/Signup Route");
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide username and password", 400);
+  }
+  res.send("Fake route");
 };
 
 const dashboard = (req, res) => {
