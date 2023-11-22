87660da5437118f0e14c9671e7640000482c5010
Alp Serin <alpsrn@gmail.com>
2023-11-22 17:21:56 +0300

    add token verification

--- controllers/main.js
+++ controllers/main.js
@@ -28,21 +28,34 @@ const login = async (req, res) => {
 };
 
 const dashboard = (req, res) => {
-  request.get(
-    {
-      url: "https://api.api-ninjas.com/v1/quotes",
-      headers: { "X-Api-Key": process.env.NINJA_API_KEY },
-    },
-    (error, response, body) => {
-      body = body.toString("utf8");
-      const quoteData = JSON.parse(body);
-      const quote = quoteData[0].quote;
-      res.status(200).json({
-        msg: `Hello, John Doe`,
-        secret: `Here is your quote : ${quote}`,
-      });
-    }
-  );
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    request.get(
+      {
+        url: "https://api.api-ninjas.com/v1/quotes",
+        headers: { "X-Api-Key": process.env.NINJA_API_KEY },
+      },
+      (error, response, body) => {
+        body = body.toString("utf8");
+        const quoteData = JSON.parse(body);
+        const quote = quoteData[0].quote;
+        res.status(200).json({
+          msg: `Hello, ${decoded.username}`,
+          secret: `Here is your quote : ${quote}`,
+        });
+      }
+    );
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access this route", 401);
+  }
 };
 
 module.exports = {
